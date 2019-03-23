6644f3f7f3cfb85a052fe682fc87d16099e55ad4
avinashsaraogi <46383113+avinashsaraogi@users.noreply.github.com>
2019-03-22 21:57:08 -0400

    Update TaskService.java

--- src/main/java/com/cts/task/service/TaskService.java
+++ src/main/java/com/cts/task/service/TaskService.java
@@ -2,12 +2,13 @@ package com.cts.task.service;
 
 import java.util.List;
 
+import com.cts.task.exception.RestException;
 import com.cts.task.model.Task;
 
 public interface TaskService {
 	
-	public Task addTask(Task task);
-	public Task updateTask(Task task);
+	public Task addTask(Task task) throws RestException;
+	public Task updateTask(Task task) throws RestException;
 	public void deleteTask(Long taskId);
 	public List<Task> listTasks();
 	public Task getTaskById(Long taskId);
