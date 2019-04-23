8a9176be4f68935de291faf8636ce772c1249fa8
avinashsaraogi <46383113+avinashsaraogi@users.noreply.github.com>
2019-04-23 11:28:13 -0400

    Update RestExceptionHandler.java

--- src/main/java/com/cts/task/exception/RestExceptionHandler.java
+++ src/main/java/com/cts/task/exception/RestExceptionHandler.java
@@ -4,14 +4,18 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+
+
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler  {
 	private static final String UNEXPECTED_ERROR = "Exception.unexpected";
     private final MessageSource messageSource;
     @Autowired
@@ -19,26 +23,57 @@ public class RestExceptionHandler {
         this.messageSource = messageSource;
     }
     
-   
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiError> invalidFormatException(HttpMessageNotReadableException ex, Locale locale) {
+    	    	    
+    	 String errorMessage = messageSource.getMessage("Exception.invalidFormat", null, locale);
+		ApiError apiError = 
+			      new ApiError(HttpStatus.BAD_REQUEST, errorMessage, ex.getLocalizedMessage());
+		return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
     
+    @ExceptionHandler(MethodArgumentNotValidException.class)    
+    public ResponseEntity<ApiError> methodArgumentNotValidException(MethodArgumentNotValidException ex, Locale locale) {
+    	String errorMessage = "";    	
+    	try
+    	{
+    	 errorMessage = messageSource.getMessage(ex.getBindingResult().getFieldError().getCode()+"."+ex.getBindingResult().getFieldError().getField(), null, locale);
+    	}
+    	catch(Exception e)
+    	{    		
+    		errorMessage = messageSource.getMessage(UNEXPECTED_ERROR, null, locale);    		
+    	}    	    	
+    	ex.printStackTrace();    	
+		ApiError apiError = 
+			      new ApiError(HttpStatus.BAD_REQUEST, errorMessage, ex.getLocalizedMessage());
+		return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+   
     @ExceptionHandler(RestException.class)
     public ResponseEntity<ApiError> handleRestExceptions(RestException ex, Locale locale) {
         String errorMessage = messageSource.getMessage(ex.getApiError().getMessage(), null, locale);
-        ex.printStackTrace();
-        
+        ex.printStackTrace();          
 		ApiError apiError = ex.getApiError();
-		apiError.setMessage(errorMessage);
-		System.out.println("Throwable error for test case "+ ex.getMessage());    
-		
+		apiError.setMessage(errorMessage);			
         return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiError> databaseException(DataIntegrityViolationException ex, Locale locale) {
+    	    	    
+    	 String errorMessage = messageSource.getMessage("Exception.DBError", null, locale);
+         ex.printStackTrace();          		    	
+		ApiError apiError = 
+			      new ApiError(HttpStatus.BAD_REQUEST, errorMessage, ex.getLocalizedMessage());
+		return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleExceptions(Exception ex, Locale locale) {
-        String errorMessage = messageSource.getMessage(UNEXPECTED_ERROR, null, locale);
-        ex.printStackTrace();
-        System.out.println("Throwable error for test case "+ ex.getMessage());
+        String errorMessage = messageSource.getMessage(UNEXPECTED_ERROR, null, locale);        
+        ex.printStackTrace();       
 		ApiError apiError = 
 			      new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage, ex.getMessage());
         return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    
+    
 }
